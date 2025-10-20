05f30407b80aeb38f5b5f7f3fe0a9f3046e85f42
Moahamed05 <zaafouri.amir@esprit.tn>
2025-10-21 00:10:29 +0100

    test Add Registation methode with Setter methode with multiple instruction

--- src/test/java/tn/esprit/spring/services/RegistrationServicesImplTest.java
+++ src/test/java/tn/esprit/spring/services/RegistrationServicesImplTest.java
@@ -1,5 +1,6 @@
 package tn.esprit.spring.services;
 
+import nonapi.io.github.classgraph.utils.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -7,15 +8,23 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import tn.esprit.spring.entities.Course;
 import tn.esprit.spring.entities.Registration;
+import tn.esprit.spring.entities.Skier;
 import tn.esprit.spring.entities.TypeCourse;
 import tn.esprit.spring.repositories.ICourseRepository;
 import tn.esprit.spring.repositories.IRegistrationRepository;
 import tn.esprit.spring.repositories.ISkierRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class) // Enable Mockito
 @DisplayName("RegestriationImpl Unit Test")
 class RegistrationServicesImplTest {
@@ -28,12 +37,20 @@ class RegistrationServicesImplTest {
     @InjectMocks
     RegistrationServicesImpl registrationServicesImpl;
     private Registration testRegistration;
+    private Skier testSkier;
 
     @BeforeEach
     void setUp() {
         this.testRegistration = Registration.builder()
                 .numRegistration(152L)
                 .numWeek(5)
+                .build();
+        this.testSkier = Skier.builder()
+                .numSkier(152L)
+                .lastName("Lin")
+                .firstName("Frank")
+                .city("Suisse")
+                .dateOfBirth(LocalDate.of(1999,05,15))
                 .build();
     }
 
@@ -46,13 +63,29 @@ class RegistrationServicesImplTest {
         @DisplayName("Shouled we Create new Registriation")
         void shouldAddRegistrationSuccefully(){
             //Given
-            Registration registration;
             Long numSkier = 15L;
-            // When
-            when()
-            //Then
+            when(skierRepository.findById(numSkier))
+                    .thenReturn(Optional.of(testSkier));
+            //3lech we don't Mock the setSkier !! khater simple Set by Ali Bouali
+            //!! why return the value we ntestiw 3lih ba3ed juste to make sure the value don't change during the execution of all methodes
+            when(registrationRepository.save(any(Registration.class)))
+                    .thenReturn(testRegistration);
 
+            // When
+            final Registration registration=  registrationServicesImpl.addRegistrationAndAssignToSkier(testRegistration,numSkier);
+
+            //Then
+            assertNotNull(registration);
+            assertEquals(testRegistration,registration);
+            verify(skierRepository,times(1)).findById(numSkier);
+            verify(registrationRepository,times(1)).save(testRegistration);
+
+            // Verify that the Skier was correctly set before we save
+            verify(registrationRepository).save(argThat(reg ->
+                    reg.getSkier() != null && reg.getSkier().getNumSkier().equals(testSkier.getNumSkier())
+            ));
         }
+
     }
 
 
